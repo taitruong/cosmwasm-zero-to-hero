ffb6cf8646c427acf08797be77f4dbee146dae71
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2022-07-26 14:10:21 +0200

    06 - instantiate contract and store config with admin passed by message caller

--- code/my-cw-starter/src/contract.rs
+++ code/my-cw-starter/src/contract.rs
@@ -1,24 +1,34 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cw2::set_contract_version;
 // use cw2::set_contract_version;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{Config, CONFIG};
 
-/*
 const CONTRACT_NAME: &str = "crates.io:my-cw-starter";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
- */
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
-    _info: MessageInfo,
-    _msg: InstantiateMsg,
+    info: MessageInfo,
+    msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    let admin = msg.admin.unwrap_or(info.sender.to_string());
+    let validated_admin = deps.api.addr_validate(&admin)?;
+    let config = Config {
+        admin: validated_admin.clone(),
+    };
+    CONFIG.save(deps.storage, &config)?;
+    Ok(Response::new()
+        .add_attribute("action", "instantiate")
+        .add_attribute("admin", validated_admin.to_string())
+    )
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -37,4 +47,10 @@ pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+
+    #[test]
+    fn test_instantiate() {
+
+    }
+}
