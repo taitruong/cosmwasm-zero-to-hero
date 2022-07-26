fe838fd4948f4eadcdbf0f146865dbd2877d5e4d
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2022-07-26 17:07:22 +0200

    08 - define execute messages for CreatePoll and Vote

--- code/my-cw-starter/src/msg.rs
+++ code/my-cw-starter/src/msg.rs
@@ -10,7 +10,15 @@ pub struct InstantiateMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    CustomMsg { val: String },
+    CreatePoll {
+        poll_id: String,
+        question: String,
+        options: Vec<String>,
+    },
+    Vote {
+        poll_id: String,
+        vote: String,
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
