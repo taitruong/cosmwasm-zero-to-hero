8e31512182ca48dbadc9e785ec9f745cea3cad2c
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2022-07-26 13:13:29 +0200

    05 - new storages/states for Config, Ballot and Poll

--- code/my-cw-starter/src/state.rs
+++ code/my-cw-starter/src/state.rs
@@ -2,12 +2,26 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
+pub struct Config {
+    pub admin: Addr,
 }
 
-pub const STATE: Item<State> = Item::new("state");
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Poll {
+    pub creator: Addr,
+    pub question: String,
+    pub options: Vec<(String, u64)>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Ballot {
+    pub option: String,
+}
+
+pub const CONFIG: Item<Config> = Item::new("state");
+
+pub const POLLS: Map<String, Poll> = Map::new("polls");
+pub const BALLOTS: Map<(Addr, String), Ballot> = Map::new("ballots");
