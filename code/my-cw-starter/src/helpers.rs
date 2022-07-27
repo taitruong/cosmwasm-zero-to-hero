5d4022c95819660eb02058da83aa2a13a0f7e3c9
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2022-07-27 15:00:49 +0200

    12 - query messages for polls and votes

--- code/my-cw-starter/src/helpers.rs
+++ code/my-cw-starter/src/helpers.rs
@@ -2,10 +2,10 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::{
-    to_binary, Addr, CosmosMsg, CustomQuery, Querier, QuerierWrapper, StdResult, WasmMsg, WasmQuery,
+    to_binary, Addr, CosmosMsg, StdResult, WasmMsg,
 };
 
-use crate::msg::{CustomResponse, ExecuteMsg, QueryMsg};
+use crate::msg::{ExecuteMsg};
 
 /// CwTemplateContract is a wrapper around Addr that provides a lot of helpers
 /// for working with this. Rename it to your contract name.
@@ -27,20 +27,4 @@ impl CwTemplateContract {
         .into())
     }
 
-    /// Get Custom
-    pub fn custom_query<Q, T, CQ>(&self, querier: &Q, val: String) -> StdResult<CustomResponse>
-    where
-        Q: Querier,
-        T: Into<String>,
-        CQ: CustomQuery,
-    {
-        let msg = QueryMsg::CustomMsg { val };
-        let query = WasmQuery::Smart {
-            contract_addr: self.addr().into(),
-            msg: to_binary(&msg)?,
-        }
-        .into();
-        let res: CustomResponse = QuerierWrapper::<CQ>::new(querier).query(&query)?;
-        Ok(res)
-    }
 }
