7527085321ee70297de96fa8d66996b7ea51a3b8
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2022-07-27 15:19:48 +0200

    13 - query for polls and votes

--- code/my-cw-starter/src/msg.rs
+++ code/my-cw-starter/src/msg.rs
@@ -1,6 +1,8 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+use crate::state::{Poll, Ballot};
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub struct InstantiateMsg {
@@ -37,3 +39,18 @@ pub enum QueryMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum MigrateMsg {}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct AllPollsResponse {
+    pub polls: Vec<Poll>,
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct PollResponse {
+    pub poll: Option<Poll>,
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct VoteResponse {
+    pub vote: Option<Ballot>,
+}
